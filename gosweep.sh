794cb01a58b9064da2ef25b4a5f06059c7be849a
Pedro Salgado <steenzout@users.noreply.github.com>
2017-04-13 09:35:01 -0600

    added gocyclo and misspell support.

--- gosweep.sh
+++ gosweep.sh
@@ -9,6 +9,8 @@
 # 5. ineffassign
 # 6. race detector
 # 7. test coverage
+# 8. gocyclo
+# 9. misspell
 #
 
 set -e
@@ -17,6 +19,9 @@ go build $(go list ./... | grep -v '/vendor/')
 
 echo 'mode: count' > profile.cov
 
+complexity="${GOCYCLO_COMPLEXITY:-5}"
+locale="${MISSPELL_LOCALE:-US}"
+max_steps=12
 
 for pkg in $(go list ./... | grep -v '/vendor/');
 do
@@ -26,7 +31,7 @@ do
 
 
     # 1. test
-    echo "processing package $dir_relative ... (1/8)"
+    echo "processing package $dir_relative... (1/$max_steps)"
     go test -v -short -covermode=count -coverprofile="$dir_relative/profile.tmp" "$dir_relative"
     if [ -f "$dir_relative/profile.tmp" ]
     then
@@ -35,41 +40,48 @@ do
     fi
 
     # 2. fmt
-    echo "processing package $dir_relative ... (2/8)"
+    echo "processing package $dir_relative... (2/$max_steps)"
     gofmt -l -w "$dir"/*.go
 
     # 3. imports
-    echo "processing package $dir_relative ... (3/8)"
+    echo "processing package $dir_relative... (3/$max_steps)"
     goimports -l -w "$dir"/*.go | tee /dev/stderr
 
     # 4. lint
-    echo "processing package $dir_relative ... (4/8)"
+    echo "processing package $dir_relative... (4/$max_steps)"
     golint $pkg | tee /dev/stderr
 
     # 5. vet
-    echo "processing package $dir_relative ... (5/8)"
+    echo "processing package $dir_relative... (5/$max_steps)"
     go vet $pkg | tee /dev/stderr
 
     # 6. ineffassign
-    echo "processing package $dir_relative ... (6/8)"
+    echo "processing package $dir_relative... (6/$max_steps)"
     ineffassign -n $dir | tee /dev/stderr
 
     # 7. race conditions
-    echo "processing package $dir_relative ... (7/8)"
+    echo "processing package $dir_relative... (7/$max_steps)"
     #env GORACE="halt_on_error=1" go test -short -race $pkg
-
-    echo "processing package $dir_relative ... (8/8)"
 
 done
 
-# test coverage
-echo 'processing code coverage...'
+# 8. gocyclo
+echo "processing package $dir_relative... (8/$max_steps)"
+find . -type f -name '*.go' -not -path './vendor/*' | xargs -I {} -P 2 gocyclo -over $complexity {}
+
+# 9. misspell over .go files
+echo "processing package $dir_relative... (9/$max_steps)"
+find . -type f -name '*.go' -not -path './vendor/*' | xargs -I {} -P 2 misspell -error -source go {}
+
+# 10. misspell over .txt .md .rst files
+echo "processing text files... (10/$max_steps)"
+find . -type f -not -path './vendor/*' \( -name '*.md' -o -name '*.txt' -o -name '*.rst' \) | xargs -I {} misspell -error -source text {}
+
+# 11. test coverage
+echo "processing code coverage... (11/$max_steps)"
 go tool cover -func profile.cov
 
-
-# To submit the test coverage result to coveralls.io,
-# use goveralls (https://github.com/mattn/goveralls)
-
+# 12. goveralls
 if [ -n "${CI_SERVICE+1}" ]; then
     echo "goveralls with ${CI_SERVICE}"
     if [ -n "${COVERALLS_TOKEN+1}" ]; then
@@ -78,3 +90,5 @@ if [ -n "${CI_SERVICE+1}" ]; then
         goveralls -coverprofile=profile.cov -service=$CI_SERVICE
     fi
 fi
+
+echo "done. (12/$max_steps)"
